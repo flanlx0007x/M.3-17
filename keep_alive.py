9969879dda8960988a132725f458d0ab0e07ce21
Responsive <104904660+flanlx0007x@users.noreply.github.com>
2025-02-26 20:22:15 +0700

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,13 +1,25 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, jsonify
 from threading import Thread
+
 app = Flask(__name__)
+
 @app.route('/')
 def index():
-    return "Alive"
+    return render_template('index.html')
+
+@app.route('/api/bot_status')
+def bot_status():
+
+    data = {
+        "status": "Online",  
+        "activity": "Coding🧑‍💻",  
+        "latency": 20 
+    }
+    return jsonify(data)
+
 def run():
-  app.run(host='0.0.0.0',port=8080)
-def keep_alive():  
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
     t = Thread(target=run)
     t.start()
-
-
